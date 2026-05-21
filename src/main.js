2aad591c1b3ac4ea0dc7d6fa22fe38ced2bd88eb
Anton-Symonenko <gan4yrnuk24@gmail.com>
2026-05-21 22:33:04 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import { getImagesByQuery } from './js/pixabay-api';
-import { createGallery, clearGallery } from './js/render-functions';
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGallery, clearGallery } from './js/render-functions.js';
 
 
 
