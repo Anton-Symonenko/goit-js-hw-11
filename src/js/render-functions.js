36efbcb60e1495e54dc10ed6957ec58668cd84c7
Anton-Symonenko <gan4yrnuk24@gmail.com>
2026-05-21 22:46:26 +0200

    123

---
+++ src/js/render-functions.js
@@ -0,0 +1,47 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const lightbox = new SimpleLightbox( '.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+    const markup = images
+        .map(({
+            webformatURL,
+            largeImageURL,
+            tags,
+            likes,
+            views,
+            comments,
+            downloads,
+        }) => `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+            />
+          </a>
+          <div class="info">
+            <p><b>Likes</b> ${likes}</p>
+            <p><b>Views</b> ${views}</p>
+            <p><b>Comments</b> ${comments}</p>
+            <p><b>Downloads</b> ${downloads}</p>
+          </div>
+        </li>
+      `
+        )
+        .join('');
+    galleryEl.insertAdjacentHTML('beforeend', markup);
+    lightbox.refresh();
+
+    
+    }
+export function clearGallery() {
+    galleryEl.innerHTML = '';
+}
